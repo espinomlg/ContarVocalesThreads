a26ffe4a654c8a70e0ad5ac4080ebb1a17365d72
Andres Espino Lopez <espinomlg@gmail.com>
2016-10-27 20:41:14 +0200

    ejercicio vocales

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,23 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		File file = new File("texto");
 		Vocales v = null;
+		VocalThread a,e,i,o,u;
 		
-		if(file.exists())
+		if(file.exists()){
 			v = new Vocales(file);
-		
-		VocalThread a = new VocalThread(v, 'a', 'A');
-		VocalThread e = new VocalThread(v, 'e', 'E');
-		VocalThread i = new VocalThread(v, 'i', 'I');
-		VocalThread o = new VocalThread(v, 'o', 'O');
-		VocalThread u = new VocalThread(v, 'u', 'U');
-		
-		a.start();e.start();i.start();o.start();u.start();
-		
-		System.out.println(v.getVocales());
 
+			a = new VocalThread(v, 'a', 'A');
+			e = new VocalThread(v, 'e', 'E');
+			i = new VocalThread(v, 'i', 'I');
+			o = new VocalThread(v, 'o', 'O');
+			u = new VocalThread(v, 'u', 'U');
+
+			a.start();e.start();i.start();o.start();u.start();
+			
+			System.out.println("El número de vocales en el texto es " + v.getVocales());
+		}
+		else
+			System.out.println("El fichero indicado no existe");
 
 	}
 
